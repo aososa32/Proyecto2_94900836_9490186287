9a7b4d056237721866a926dea0e0f9f314f832ff
aososa32 <140859888+aososa32@users.noreply.github.com>
2023-11-03 22:18:19 -0600

    Delete Backend directory

--- Backend/config.js
+++
@@ -1,9 +0,0 @@
-var config = {}
-config.puerto = 3001;
-config.claveoculta = "prueba123"
-config.tiempodesesion = (60000 *60) //60 segundos por 60 es una hora
-config.dbMongo = "Tienda"
-config.Uri = 'mongodb+srv://asosad:I8NDJni2L5hqZovX@asosaumg.ewgxfqc.mongodb.net/Tienda?retryWrites=true&w=majority';
-// para consultar o crear una DB especifica y no se inserte en TEST se debe colcoar el nombre de la DB luego de este parte
-// mongodb.net/Tienda?re
-module.exports.config = config
